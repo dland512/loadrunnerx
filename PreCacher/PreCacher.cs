496f1d57cf905966efaa80b2b2896dbd5133562e
David Land <davidl@eurekasoft.com>
2015-08-18 22:34:11 -0500

    added pre-cacher

---
+++ PreCacher/PreCacher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PreCacher.localhost;
+
+namespace PreCacher
+{
+    public class PreCacher
+    {
+        private MainService service;
+
+        public PreCacher(MainService service)
+        {
+            this.service = service;
+        }
+
+
+        public void CacheData(long vendorID, long jobID, DateTime start, DateTime end)
+        {
+            Console.WriteLine("caching data for job {0}, times {1} - {2}", jobID, start.ToString("yyyy-MM-dd HH:mm:ss"), end.ToString("yyyy-MM-dd HH:mm:ss"));
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+
+            //convert(dateTime,'2015-08-13 13:52:00',120)
+
+            string where = string.Format("WHERE Vendor_ID = {0} AND Job_ID = {1} AND " +
+                "Last_Updated BETWEEN convert(dateTime,'{2}',120) AND convert(dateTime,'{3}',120)",
+                vendorID, jobID, start.ToString("yyyy-MM-dd HH:mm:ss"), end.ToString("yyyy-MM-dd HH:mm:ss"));
+
+            Console.WriteLine(where);
+
+            int total;
+            service.SelectPipesWithoutImageDataWithWelds(where, "Pipe_ID", "ASC", "0", "0", out total);
+            int numPages = (int)Math.Ceiling(total / 1000.0);
+
+            for (int i = 0; i < numPages; i++)
+            {
+                Pipe[] pipes = service.SelectPipesWithoutImageDataWithWelds(where, "Pipe_ID", "ASC", i.ToString(), "1000", out total);
+                Console.WriteLine("Retrieved {0} pipes for page {1} under job {2}: {3}", pipes.Length, i, jobID, watch.Elapsed);
+            }
+
+            watch.Stop();   
+        }
+    }
+}
---
+++ PreCacher/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+using PreCacher.localhost;
+
+namespace PreCacher
+{
+    class Program
+    {
+        private MainService service;
+
+        public class CommandLineArgs
+        {
+            [Option('u', "username", Required = true, HelpText = "Username for web service calls")]
+            public string Username { get; set; }
+
+            [Option('p', "password", Required = true, HelpText = "Password for web service calls")]
+            public string Password { get; set; }
+
+            [Option('v', "vendorid", Required = true, HelpText = "Vendor ID")]
+            public long VendorID { get; set; }
+
+            [Option('j', "jobid", Required = true, HelpText = "Job ID")]
+            public long JobID { get; set; }
+
+            [Option('r', "repeat", Required = true, HelpText = "How often (in minutes) to cache data")]
+            public int RepeatMin { get; set; }
+
+            [Option('t', "topofhour", Required = false, HelpText = "Wait until the top of the hour to start caching")]
+            public bool TopOfHour { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            CommandLineArgs parsedArgs = new CommandLineArgs();
+            bool ok = CommandLine.Parser.Default.ParseArguments(args, parsedArgs);
+
+            if (!ok)
+            {
+                HelpText help = HelpText.AutoBuild(parsedArgs);
+                throw new Exception(string.Format("Invalid command line args: {0}", help.ToString()));
+            }
+
+            if (parsedArgs.TopOfHour)
+            {
+                //wait until the top of the hour to start
+                TimeSpan ts = TimeUntilNextHour();
+                Console.WriteLine("waiting for " + ts);
+                Thread.Sleep(ts);
+            }
+
+            MainService service = GetService(parsedArgs.Username, parsedArgs.Password);
+            PreCacher cacher = new PreCacher(service);
+            
+            while (true)
+            {
+                //cache the last hour of changes
+                DateTime now = DateTime.Now;
+                DateTime start = GetDateTimeToHour(now.AddHours(-1));
+                DateTime end = GetDateTimeToHour(now);
+                cacher.CacheData(parsedArgs.VendorID, parsedArgs.JobID, start, end);
+
+                //wait some number of minutes
+                Thread.Sleep(parsedArgs.RepeatMin * 60 * 1000);
+            }
+        }
+
+
+        private static MainService GetService(string username, string password)
+        {
+            MainService service = new MainService();
+            service.Url = ConfigurationManager.AppSettings["ServiceUrl"];
+            service.ServiceHeaderValue = new ServiceHeader() { Username = username, Password = password, ApiVersion = "release7" };
+            service.Timeout = 100000000;
+            return service;
+        }
+
+
+        private static DateTime GetDateTimeToHour(DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, dt.Day, dt.Hour, 0, 0);
+        }
+
+
+        private static TimeSpan TimeUntilNextHour()
+        {
+            DateTime now = DateTime.Now;
+            DateTime nextHour = GetDateTimeToHour(now.AddHours(1));
+            return nextHour.Subtract(now);
+        }
+    }
+}
